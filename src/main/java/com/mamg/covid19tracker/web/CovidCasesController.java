0c10b937f832e9a5b6a29f833e4809dc21b9e9f4
Amghar <mouhcine.amghar@gmail.com>
2021-11-19 13:16:41 +0100

    create the controller

--- src/main/java/com/mamg/covid19tracker/web/CovidCasesController.java
+++ src/main/java/com/mamg/covid19tracker/web/CovidCasesController.java
@@ -4,65 +4,70 @@ import com.mamg.covid19tracker.entities.CovidCases;
 import com.mamg.covid19tracker.enums.Countries;
 import com.mamg.covid19tracker.enums.PreDateType;
 import com.mamg.covid19tracker.enums.TestResult;
-import com.mamg.covid19tracker.repositories.CovidCasesRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import com.mamg.covid19tracker.service.CovidCasesService;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
-public class CovidCasesController {
-    @Autowired
-    private CovidCasesRepository covidCasesRepository;
+public class CovidCasesController extends ResponseEntityExceptionHandler {
+
+    private final CovidCasesService covidCasesService;
+
+    public CovidCasesController(CovidCasesService covidCasesService) {
+        this.covidCasesService = covidCasesService;
+    }
 
     @PostMapping(path = "/covid-cases")
-    public CovidCases save(@RequestBody CovidCases cases) throws Exception {
-        return covidCasesRepository.save(cases);
+    @ResponseStatus(HttpStatus.CREATED)
+    public CovidCases save(@Valid @RequestBody CovidCases cases) throws Exception {
+        return covidCasesService.addCase(cases);
     }
 
     @PutMapping(path = "/covid-cases/{id}")
-    public CovidCases update(@RequestParam("id") Long id, @RequestBody CovidCases cases) throws Exception {
-        CovidCases c = covidCasesRepository.findById(id).get();
-        if (c == null) {
-            throw new Exception("Not Existing case");
-        }
-        cases.setId(id);
-        return covidCasesRepository.save(cases);
+    @ResponseStatus(HttpStatus.CREATED)
+    public CovidCases update(@RequestParam("id") @Min(1) Long id, @Valid @RequestBody CovidCases cases) throws Exception {
+        return covidCasesService.updateCase(id,cases);
     }
 
     @GetMapping(path = "/covid-cases")
-    public ResponseEntity<Map<String, Object>> getCase(@Param("date") Date date,
-                                                       @Param("type") PreDateType type,
-                                                       @Param("date") Countries[] countries,
-                                                       @RequestParam(defaultValue = "0") int page,
-                                                       @RequestParam(defaultValue = "3") int size) throws Exception {
-        Pageable paging = PageRequest.of(page, size);
+    public ResponseEntity<Map<String, Object>> getCase(@Param("date") @NotBlank(message = "Please provide a date") String date,
+                                                       @Param("type") @NotNull(message = "Please provide a PreDateType") PreDateType type,
+                                                       @Param("countries") @NotNull(message = "Please provide a Countries")  Countries[] countries,
+                                                       @Param("test_result")  @NotNull(message = "Please provide a TestResult") TestResult testResult,
+                                                       @RequestParam(defaultValue = "0") @Min(value = 0,message ="Page index must be 0 or higher" ) int page,
+                                                       @RequestParam(defaultValue = "3") @Min(value = 1,message = "Page size must be 1 or higher") int size) throws Exception {
 
-        Page<CovidCases> p;
-        switch (type) {
-            case DAY:
-                p=covidCasesRepository.getCovidCasesByDate(date, countries, paging);
-                break;
-            case MONTH:
-                p=covidCasesRepository.getCovidCasesByMonth(date, countries, paging);
-                break;
-            default:
-                p=covidCasesRepository.getCovidCasesByYear(date, countries, paging);
 
-        }
-        Map<String, Object> response = new HashMap<>();
-        List <CovidCases> results=p.getContent();
-        response.put("cases", results);
-        response.put("currentPage", p.getNumber());
-        response.put("totalItems", p.getTotalElements());
-        response.put("totalPages", p.getTotalPages());
+        return covidCasesService.getCases(date,type,countries,testResult,page,size);
+    }
 
-        return new ResponseEntity<>(response, HttpStatus.OK);
+
+    @Override
+    @ExceptionHandler({ ConstraintViolationException.class })
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        Map<String,Object> body=new LinkedHashMap<>();
+        body.put("timestamp",new Date());
+        body.put("status",status.value());
+
+        List<String> errors= ex.getBindingResult().getFieldErrors().stream()
+                .map(x -> x.getDefaultMessage()).collect(Collectors.toList());
+
+        body.put("errors",errors);
+        return new ResponseEntity<>(body, headers, status);
     }
 }
--- src/main/java/com/mamg/covid19tracker/web/VaccinationController.java
+++ src/main/java/com/mamg/covid19tracker/web/VaccinationController.java
@@ -1,30 +1,56 @@
 package com.mamg.covid19tracker.web;
 
 
+import com.mamg.covid19tracker.entities.CovidCases;
 import com.mamg.covid19tracker.entities.Vaccination;
+import com.mamg.covid19tracker.enums.Countries;
+import com.mamg.covid19tracker.enums.PreDateType;
+import com.mamg.covid19tracker.enums.TestResult;
+import com.mamg.covid19tracker.enums.VaccineType;
 import com.mamg.covid19tracker.repositories.VaccinationRepository;
+import com.mamg.covid19tracker.service.VaccinationService;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class VaccinationController {
-    private VaccinationRepository vaccinationRepository;
+    private VaccinationService vaccinationService;
 
-    public VaccinationController(VaccinationRepository vaccinationRepository) {
-        this.vaccinationRepository = vaccinationRepository;
+    public VaccinationController(VaccinationService vaccinationService) {
+        this.vaccinationService = vaccinationService;
     }
 
     @PostMapping(path = "/vaccinations")
     public Vaccination save(@RequestBody Vaccination vaccination) throws Exception {
-        return vaccinationRepository.save(vaccination);
+        return vaccinationService.addVaccine(vaccination);
     }
 
     @PutMapping(path = "/vaccinations/{id}")
     public Vaccination update(@RequestParam("id") Long id, @RequestBody Vaccination vaccination) throws Exception {
-        Vaccination v= vaccinationRepository.findById(id).get();
-        if(v==null){
-            throw new Exception("Not Existing case");
-        }
-        vaccination.setId(id);
-        return vaccinationRepository.save(vaccination);
+
+        return vaccinationService.updateVaccine(id,vaccination);
+    }
+
+
+    @GetMapping(path = "/vaccinations")
+    public ResponseEntity<Map<String, Object>> getVaccines(@Param("date") @NotBlank(message = "Please provide a date") String date,
+                                                       @Param("type") @NotNull(message = "Please provide a PreDateType") PreDateType type,
+                                                       @Param("date") @NotNull(message = "Please provide a Countries")  Countries[] countries,
+                                                       @Param("name")  @NotNull(message = "Please provide a TestResult") VaccineType name,
+                                                       @RequestParam(defaultValue = "false")  boolean isSecondVaccine,
+                                                       @RequestParam(defaultValue = "0") @Min(value = 0,message ="Page index must be 0 or higher" ) int page,
+                                                       @RequestParam(defaultValue = "3") @Min(value = 1,message = "Page size must be 1 or higher") int size) throws Exception {
+
+
+        return vaccinationService.getVaccineOfDay(date,countries,type,name,isSecondVaccine,page,size);
     }
 }
