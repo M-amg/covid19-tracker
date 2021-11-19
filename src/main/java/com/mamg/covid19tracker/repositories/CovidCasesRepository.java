c1bc578431929614f91a826146b52375d4a8598b
Amghar <mouhcine.amghar@gmail.com>
2021-11-19 13:15:59 +0100

    add methode get case by date an get vaccine by date to repo

--- src/main/java/com/mamg/covid19tracker/repositories/CovidCasesRepository.java
+++ src/main/java/com/mamg/covid19tracker/repositories/CovidCasesRepository.java
@@ -3,6 +3,7 @@ package com.mamg.covid19tracker.repositories;
 
 import com.mamg.covid19tracker.entities.CovidCases;
 import com.mamg.covid19tracker.enums.Countries;
+import com.mamg.covid19tracker.enums.TestResult;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,13 +12,16 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.Date;
 
-public interface CovidCasesRepository extends JpaRepository<CovidCases,Long> {
+public interface CovidCasesRepository extends JpaRepository<CovidCases, Long> {
+    boolean existsById(Long id);
 
-    @Query("SELECT COUNT(C),C.country FROM CovidCases C WHERE C.testResult='POSITIVE' AND  C.testDate = :date AND C.country in (:countries) GROUP BY (C.country)")
-    public Page<CovidCases> getCovidCasesByDate(@Param("date") Date date, @Param("countries") Countries[] countries, Pageable pageable);
-    @Query("SELECT COUNT(C),C.country FROM CovidCases C WHERE C.testResult='POSITIVE' AND SUBSTRING(C.testDate,5, 7)=SUBSTRING(:date,5, 7) AND SUBSTRING(C.testDate,0, 4)=SUBSTRING(:date,0, 4)  AND C.country in (:countries) GROUP BY (C.country)")
-    public Page<CovidCases> getCovidCasesByMonth(@Param("date") Date date, @Param("countries") Countries[] countries, Pageable pageable);
-    @Query("SELECT COUNT(C),C.country FROM CovidCases C WHERE C.testResult='POSITIVE' AND SUBSTRING(C.testDate,0, 4)=SUBSTRING(:date,0, 4)  AND C.country in (:countries) GROUP BY (C.country)")
-    public Page<CovidCases> getCovidCasesByYear(@Param("date") Date date, @Param("countries") Countries[] countries, Pageable pageable);
+    @Query("SELECT COUNT(C),C.country FROM CovidCases C WHERE C.testResult=:testResult AND  SUBSTRING(C.testDate,0,10) = SUBSTRING(:date,0,10)  AND C.country in (:countries) GROUP BY (C.country)")
+    public Page<CovidCases> getCovidCasesByDate(@Param("date") String date, @Param("countries") Countries[] countries, @Param("testResult") TestResult testResult, Pageable pageable);
+
+    @Query("SELECT COUNT(C),C.country FROM CovidCases C WHERE C.testResult=:testResult AND SUBSTRING(C.testDate,0, 7)=SUBSTRING(:date,0, 7) AND  C.country in (:countries) GROUP BY (C.country)")
+    public Page<CovidCases> getCovidCasesByMonth(@Param("date") String date, @Param("countries") Countries[] countries, @Param("testResult") TestResult testResult,Pageable pageable);
+
+    @Query("SELECT COUNT(C),C.country FROM CovidCases C WHERE C.testResult=:testResult AND SUBSTRING(C.testDate,0, 4)=SUBSTRING(:date,0, 4)  AND C.country in (:countries) GROUP BY (C.country)")
+    public Page<CovidCases> getCovidCasesByYear(@Param("date") String date, @Param("countries") Countries[] countries, @Param("testResult") TestResult testResult,Pageable pageable);
 
 }
--- src/main/java/com/mamg/covid19tracker/repositories/VaccinationRepository.java
+++ src/main/java/com/mamg/covid19tracker/repositories/VaccinationRepository.java
@@ -1,7 +1,37 @@
 package com.mamg.covid19tracker.repositories;
 
 import com.mamg.covid19tracker.entities.Vaccination;
+import com.mamg.covid19tracker.entities.Vaccination;
+import com.mamg.covid19tracker.enums.Countries;
+import com.mamg.covid19tracker.enums.TestResult;
+import com.mamg.covid19tracker.enums.VaccineType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface VaccinationRepository extends JpaRepository<Vaccination,Long> {
+
+    boolean existsById(Long id);
+
+    @Query("SELECT COUNT(C),C.country FROM Vaccination C WHERE C.typeName=:typeName AND  SUBSTRING(C.vaccineDate,0,10) = SUBSTRING(:date,0,10)  AND C.country in (:countries) GROUP BY (C.country)")
+    public Page<Vaccination> getVaccinationByDate(@Param("date") String date, @Param("countries") Countries[] countries, @Param("typeName") VaccineType typeName, Pageable pageable);
+
+    @Query("SELECT COUNT(C),C.country FROM Vaccination C WHERE C.typeName=:typeName AND SUBSTRING(C.vaccineDate,0, 7)=SUBSTRING(:date,0, 7) AND  C.country in (:countries) GROUP BY (C.country)")
+    public Page<Vaccination> getVaccinationByMonth(@Param("date") String date, @Param("countries") Countries[] countries, @Param("typeName") VaccineType typeName, Pageable pageable);
+
+    @Query("SELECT COUNT(C),C.country FROM Vaccination C WHERE C.typeName=:typeName AND SUBSTRING(C.vaccineDate,0, 4)=SUBSTRING(:date,0, 4)  AND C.country in (:countries) GROUP BY (C.country)")
+    public Page<Vaccination> getVaccinationByYear(@Param("date") String date, @Param("countries") Countries[] countries, @Param("typeName") VaccineType typeName, Pageable pageable);
+
+    @Query("SELECT COUNT(C),C.country FROM Vaccination C WHERE C.typeName=:typeName AND  SUBSTRING(C.vaccine2Date,0,10) = SUBSTRING(:date,0,10)  AND C.country in (:countries) GROUP BY (C.country)")
+    public Page<Vaccination> getSecondVaccinationByDate(@Param("date") String date, @Param("countries") Countries[] countries, @Param("typeName") VaccineType typeName, Pageable pageable);
+
+    @Query("SELECT COUNT(C),C.country FROM Vaccination C WHERE C.typeName=:typeName AND SUBSTRING(C.vaccine2Date,0, 7)=SUBSTRING(:date,0, 7) AND  C.country in (:countries) GROUP BY (C.country)")
+    public Page<Vaccination> getSecondVaccinationByMonth(@Param("date") String date, @Param("countries") Countries[] countries, @Param("typeName") VaccineType typeName, Pageable pageable);
+
+    @Query("SELECT COUNT(C),C.country FROM Vaccination C WHERE C.typeName=:typeName AND SUBSTRING(C.vaccine2Date,0, 4)=SUBSTRING(:date,0, 4)  AND C.country in (:countries) GROUP BY (C.country)")
+    public Page<Vaccination> getSecondVaccinationByYear(@Param("date") String date, @Param("countries") Countries[] countries, @Param("typeName") VaccineType typeName, Pageable pageable);
+
+
 }
